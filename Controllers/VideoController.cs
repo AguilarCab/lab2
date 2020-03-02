a6ad83e06d63ce3ba9ddfc09e0a3c32444feb4ff
Saul Alejandro Aguilar Caballero <saul.aguilar.caballero@gmail.com>
2020-03-02 11:05:43 -0600

    Registro Video

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using System.Data;
+using System.Data.SqlClient;
+using MVCLaboratorio.Utilerias;
+
+
+namespace MVCLaboratorio.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()//Ver video//
+        {
+            return View();
+        }
+
+        public ActionResult Create()//crear video, muestra la interfaz//
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(int idVideo, string titulo, int repro, string url)
+        {
+
+            //guardar video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+
+
+            BaseHelper.ejecutarSentencia("sp_video_insertar", CommandType.StoredProcedure, parametros);
+
+
+            return RedirectToAction("Index","Video");
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Delete(int idVideo)
+        {
+            return View();
+        }
+
+        public ActionResult Edit()//
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int idVideo, string titulo, int repro, string url)
+        {
+            return View();
+        }
+
+
+    }
+}
